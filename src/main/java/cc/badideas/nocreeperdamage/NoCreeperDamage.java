05f9e4c987db654d14034a0b4a9d1d9a770768aa
Micah <micah.apex@gmail.com>
2026-05-06 14:07:26 -0400

    Update to MC 26.1.2

--- src/main/java/cc/badideas/nocreeperdamage/NoCreeperDamage.java
+++ src/main/java/cc/badideas/nocreeperdamage/NoCreeperDamage.java
@@ -1,9 +1,10 @@
 package cc.badideas.nocreeperdamage;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
-import net.minecraft.world.GameRules;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleBuilder;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.gamerules.GameRule;
+import net.minecraft.world.level.gamerules.GameRuleCategory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,9 @@ public class NoCreeperDamage implements ModInitializer {
     public static final String MOD_ID = "nocreeperdamage";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final GameRules.Key<GameRules.BooleanRule> CREEPER_BLOCK_DAMAGE = GameRuleRegistry.register("creeperBlockDamage", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
+    public static final GameRule<Boolean> CREEPER_BLOCK_DAMAGE = GameRuleBuilder.forBoolean(true)
+            .category(GameRuleCategory.MOBS)
+            .buildAndRegister(Identifier.fromNamespaceAndPath(MOD_ID, "creeper_block_damage"));
 
     @Override
     public void onInitialize() {
--- src/main/java/cc/badideas/nocreeperdamage/mixin/ExplosionImplMixin.java
+++ src/main/java/cc/badideas/nocreeperdamage/mixin/ExplosionImplMixin.java
@@ -1,14 +1,14 @@
 package cc.badideas.nocreeperdamage.mixin;
 
 import cc.badideas.nocreeperdamage.NoCreeperDamage;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.explosion.Explosion;
-import net.minecraft.world.explosion.ExplosionBehavior;
-import net.minecraft.world.explosion.ExplosionImpl;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.ExplosionDamageCalculator;
+import net.minecraft.world.level.ServerExplosion;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -17,28 +17,28 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ExplosionImpl.class)
+@Mixin(ServerExplosion.class)
 public class ExplosionImplMixin {
     @Shadow @Final
     @Mutable
-    private Explosion.DestructionType destructionType;
+    private Explosion.BlockInteraction blockInteraction;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void afterConstructor(
-            ServerWorld world,
+            ServerLevel world,
             Entity entity,
             DamageSource damageSource,
-            ExplosionBehavior behavior,
-            Vec3d pos,
+            ExplosionDamageCalculator behavior,
+            Vec3 pos,
             float power,
             boolean createFire,
-            Explosion.DestructionType destructionType,
+            Explosion.BlockInteraction destructionType,
             CallbackInfo ci
     ) {
-        if (entity instanceof CreeperEntity) {
-            boolean allowCreeperDamage = world.getGameRules().getBoolean(NoCreeperDamage.CREEPER_BLOCK_DAMAGE);
+        if (entity instanceof Creeper) {
+            boolean allowCreeperDamage = world.getGameRules().get(NoCreeperDamage.CREEPER_BLOCK_DAMAGE);
             if (!allowCreeperDamage) {
-                this.destructionType = Explosion.DestructionType.KEEP;
+                this.blockInteraction = Explosion.BlockInteraction.KEEP;
             }
         }
     }
